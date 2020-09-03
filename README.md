404b6cbb51494cb9af6f19e706b37fcea4dd985f
Steven Black <genseb7@gmail.com>
2020-09-03 10:09:26 -0400

    add goods

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # terraform-provider-errorcheck-examples
+
+
+## Installing 
+
+
---
+++ main.tf
@@ -0,0 +1,18 @@
+
+terraform {
+  required_providers {
+    errorcheck = {
+      source = "rhythmictech/errorcheck"
+      version = "2.0.4-rc3"
+    }
+  }
+}
+
+resource "errorcheck_is_valid" "workspace_should_match_env" {
+  name = "terraform workspace should match env variable"
+  test = {
+    assert        = terraform.workspace == "default"
+    error_message = "Change workspace to match env. Workspace: ${terraform.workspace}, env: default"
+  }
+}
+
